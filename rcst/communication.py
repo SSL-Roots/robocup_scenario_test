c369108a08448b68353c555d023efe4c5d107975
Shota Aoki <macakasit@gmail.com>
2024-01-28 23:30:28 +0900

    Add methods to the Communication class to change the grsim world directly (#6)
    * Communication class has methods to change the grsim world directly
    
    * Update README
    
    * Update to pass lint

--- rcst/communication.py
+++ rcst/communication.py
@@ -46,6 +46,38 @@ class Communication:
         self.referee.set_command(command)
         time.sleep(sleep_time)
 
+    def send_empty_world(self, sleep_time: float = 0.1):
+        print("Send empty world.")
+        world = SimWorld.make_empty_world()
+        self.send_replacement(world)
+        time.sleep(sleep_time)
+
+    def send_ball(self, x: float, y: float, v_x: float = 0.0, v_y: float = 0.0,
+                  sleep_time: float = 0.1):
+        print("Send ball at ({}, {}) with velocity ({}, {}).".format(x, y, v_x, v_y))
+        world = SimWorld()
+        world.set_ball(x, y, v_x, v_y)
+        self.send_replacement(world)
+        time.sleep(sleep_time)
+
+    def send_blue_robot(self, robot_id: int, x: float, y: float, orientation: float,
+                        sleep_time: float = 0.1):
+        print("Send blue robot {} at ({}, {}) with orientation {}.".format(
+            robot_id, x, y, orientation))
+        world = SimWorld()
+        world.set_blue_robot(robot_id, x, y, orientation)
+        self.send_replacement(world)
+        time.sleep(sleep_time)
+
+    def send_yellow_robot(self, robot_id: int, x: float, y: float, orientation: float,
+                          sleep_time: float = 0.1):
+        print("Send yellow robot {} at ({}, {}) with orientation {}.".format(
+            robot_id, x, y, orientation))
+        world = SimWorld()
+        world.set_yellow_robot(robot_id, x, y, orientation)
+        self.send_replacement(world)
+        time.sleep(sleep_time)
+
     def _vision_update(self):
         vision_world = VisionWorld()
         while self._thread_running:
