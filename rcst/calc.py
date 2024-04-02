ded97512588f1ca5acdc79edec83c3499148af77
nabekuro <38589340+nabekuro0104@users.noreply.github.com>
2024-04-02 23:15:26 +0900

    Add function to find distance between line segment AB and point C  (#19)
    * 線分ABと点Cの距離を求める関数追加
    
    * 対応：W293 blank line contains whitespace
    
    * 余計な処理削除
    
    * テスト追加
    
    * テスト修正
    
    * 処理修正
    
    * lint対応
    
    * lint対応
    
    * テストパターン追加
    
    * Update rcst/calc.py
    
    Co-authored-by: Shota Aoki <macakasit@gmail.com>
    
    * Update rcst/calc.py
    
    Co-authored-by: Shota Aoki <macakasit@gmail.com>
    
    * Update rcst/calc.py
    
    Co-authored-by: Shota Aoki <macakasit@gmail.com>
    
    * Update rcst/calc.py
    
    Co-authored-by: Shota Aoki <macakasit@gmail.com>
    
    * Update tests/test_calc.py
    
    Co-authored-by: Shota Aoki <macakasit@gmail.com>
    
    * fix comment
    
    ---------
    
    Co-authored-by: Shota Aoki <macakasit@gmail.com>

--- rcst/calc.py
+++ rcst/calc.py
@@ -32,3 +32,29 @@ def velocity_norm(present: Robot, previous: Robot, dt: float) -> float:
     vx = (present.x - previous.x) / dt
     vy = (present.y - previous.y) / dt
     return math.sqrt(vx**2 + vy**2)
+
+
+def calc_vector(x0: float, y0: float, x1: float, y1: float) -> tuple[float, float]:
+    return x1-x0, y1-y0
+
+
+def distance_line_ab_to_point_c(xa: float, ya: float, xb: float, yb: float, xc: float, yc: float) -> float:
+    ACx, ACy = calc_vector(xa, ya, xc, yc)
+    ABx, ABy = calc_vector(xa, ya, xb, yb)
+    BCx, BCy = calc_vector(xb, yb, xc, yc)
+    BAx, BAy = calc_vector(xb, yb, xa, ya)
+
+    ab_is_not_line = False
+    if xa == xb and ya == yb:
+        ab_is_not_line = True
+
+    # AB is point or point C is the A side
+    if ab_is_not_line or ACx * ABx + ACy * ABy < 0:
+        return (ACx * ACx + ACy * ACy)**0.5
+    # Point C is the B side
+    if BCx * BAx + BCy * BAy < 0:
+        return (BCx * BCx + BCy * BCy)**0.5
+
+    v_s = abs(ACx * ABy - ACy * ABx)
+    v_l = (ABx * ABx + ABy * ABy)**0.5
+    return v_s/v_l
